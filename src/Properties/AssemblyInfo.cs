8f04946bbdc7004315fd2d2a7cb0668004cedb23
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-10-08 18:42:55 +0200

    Versioning did not work

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -25,5 +27,6 @@
 //      Build Number
 //      Revision
 //
-// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.*")]
 // [assembly: AssemblyFileVersion("1.0.0.0")]
